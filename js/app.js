877dffc0d79072aaa0318244f1ac7fae7cad1f81
Paulina Dabrowska <14799673+Trufs@users.noreply.github.com>
2018-04-04 19:01:46 +0200

    feat: add move counter

--- js/app.js
+++ js/app.js
@@ -19,7 +19,6 @@ const listOfCards = ['<i class="fa fa-anchor"></i>', '<i class="fa fa-bicycle"><
 
 }
 
-
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
@@ -39,15 +38,21 @@ function shuffle(array) {
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
  *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
-
  *  - if the list already has another card, check to see if the two cards match
  *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
-
+const movesDisplay = document.querySelector('.moves');
 let openCards = [];
+let moveCounter = 0;
+
+function incrementMoves(){
+	moveCounter += 1;
+	movesDisplay.textContent = moveCounter;
+}
 
 function openCard(card){
 	card.classList.toggle('open'); //it works
@@ -67,48 +72,48 @@ function lockCardsOpen(listOfCards){ //it works
 		const oneCard=listOfCards[i];
 		oneCard.classList.add('match');
 	}
-
 	openCards = [];
+	incrementMoves();
 }
 
 function hideCards(listOfCards){
-	console.log('hello');			//this works
 	for (let i =0;i<listOfCards.length;i++){
-		console.log('hi');	//works
 		const oneCard=listOfCards[i];
 		oneCard.classList.toggle('show');
 		oneCard.classList.toggle('open');
 	}
-
 	openCards = [];
+	incrementMoves();
 }
 
+
 function activeCard(card){
-	let cardSymbol = card.querySelector("i");
 	openCard(card);
 	displaySymbol(card);
 	addToList(card);
 
 	if (openCards.length>1){
-		if (openCards[0].innerHTML === openCards[1].innerHTML){ //this works
-			if(openCards[0] !== openCards[1]){ //prevents adding matched class to a card with double clicking
+
+		if (openCards[0].innerHTML === openCards[1].innerHTML){
+			if(openCards[0] !== openCards[1]){  //prevents adding matched class to a card with double clicking
 			console.log("same");
 			lockCardsOpen(openCards);
+			}
 		}
-		}
+
 		else {
 			deck.addEventListener('dblclick', respondToDblClick);
 
 		}
-			}
+	}
 }
 
-const deck = document.querySelector('.deck');
 
+
+const deck = document.querySelector('.deck');
 deck.addEventListener('click', respondToTheClick);
 
 //functions to handle Events
-
 function respondToTheClick(event){
 	activeCard(event.target);
 }
